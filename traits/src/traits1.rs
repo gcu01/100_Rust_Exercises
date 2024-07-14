5621897c208f705e9dcf9f77cc2ca566dff7ee15
Gabriel <gabrielcampeanu@gmail.com>
2024-07-14 15:05:02 +0200

    add trait support for u32 and i32

--- traits/src/traits1.rs
+++ traits/src/traits1.rs
@@ -24,10 +24,29 @@ mod mytraits {
     }
 }
 
+mod unsigned_no {
+    use crate::traits1::IsEven;
+    impl IsEven for u32 {
+        fn is_even(&self) -> bool {
+            self%2 == 0
+        }
+    }
+}
+
+mod integer_no {
+    use crate::traits1::IsEven;
+    impl IsEven for i32 {
+        fn is_even(&self) -> bool {
+            self %2 == 0
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::mytraits::{MyTrait};
     use crate::traits1::IsEven;
+    //use crate::traits1::unsigned_no;
 
     #[test]
     fn test1_is_even(){
@@ -38,5 +57,14 @@ mod tests {
     fn test2_is_even() {
         let v:MyTrait = MyTrait::new(5);
         assert_eq!(false, v.is_even());
+    }
+
+    #[test]
+    fn test3_is_even() {
+        assert_eq!(true, 4_u32.is_even());
+    }
+    #[test]
+    fn test4_is_even() {
+        assert!(!5_i32.is_even());
     }
 }
