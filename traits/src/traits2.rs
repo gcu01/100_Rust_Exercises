f36a9a925ba98da03ff72c9d5e615f85b0d0fd6e
Gabriel <gabrielcampeanu@gmail.com>
2024-07-15 03:37:49 +0200

    error when implementing a trait several times

---
+++ traits/src/traits2.rs
@@ -0,0 +1,16 @@
+pub trait PartialEq {
+    fn eq(&self, b:&Self) ->bool;
+}
+impl PartialEq for u32 {
+    fn eq(&self, b:&Self) -> bool {
+        *self == *b
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn test_eq() {
+        assert!(5_u32.eq(5_u32));
+    }
+}
