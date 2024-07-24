e482f9c00ebd5850d7a36dd7dcce317828d8a9a3
Gabriel <gabrielcampeanu@gmail.com>
2024-07-24 19:02:33 +0200

    error enums

--- ticket_v2/src/main.rs
+++ ticket_v2/src/main.rs
@@ -1,9 +1,13 @@
+mod helpers;
 //mod ticket1;
 //mod ticket2;
 //mod ticket3;
 //mod ticket4;
-mod ticket6;
+//mod ticket6;
+//mod ticket7;
+mod ticket8;
 
 fn main() {
     println!("Hello, world!");
 }
+
