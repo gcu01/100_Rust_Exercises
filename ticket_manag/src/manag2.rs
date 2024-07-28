963088099c0103227d9cbeb4c5e8e054c62000ce
Gabriel <gabrielcampeanu@gmail.com>
2024-07-28 08:46:58 +0200

    automatically resizing the vector

--- ticket_manag/src/manag2.rs
+++ ticket_manag/src/manag2.rs
@@ -59,4 +59,19 @@ mod tests{
     fn test_fibonacci6(){
         assert_eq!(fibonacci(30), 832040);
     }
+
+    #[test]
+    fn resizing() {
+        let mut v = Vec::with_capacity(2);
+        v.push(1);
+        v.push(2); // max capacity reached
+        assert_eq!(v.capacity(), 2);
+
+        v.push(3); // beyond capacity, needs to resize
+
+        // Can you guess what the new capacity will be?
+        // Beware that the standard library makes no guarantees about the
+        // algorithm used to resize the vector, so this may change in the future.
+        assert_eq!(v.capacity(), 4);
+    }
 }
